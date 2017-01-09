3191c711a0b5211e987a371a0c8f33f0983e8acf
littlepod <Rahul@Mac-mini-Om.local>
2017-01-09 14:05:46 +0530

    sample code for test

---
+++ ThreeRingControlSDK/ThreeRingControlSDK.h
@@ -0,0 +1,19 @@
+//
+//  ThreeRingControlSDK.h
+//  ThreeRingControlSDK
+//
+//  Created by Om Prakash on 12/21/16.
+//  Copyright © 2016 Om. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThreeRingControlSDK.
+FOUNDATION_EXPORT double ThreeRingControlSDKVersionNumber;
+
+//! Project version string for ThreeRingControlSDK.
+FOUNDATION_EXPORT const unsigned char ThreeRingControlSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThreeRingControlSDK/PublicHeader.h>
+
+
